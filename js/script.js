d123d771f7fbfd7570e421f3280325ddd0b0351a
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-02-09 22:16:48 +0100

    chore: initial commit
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function add(lv, rv) {
+  return lv + rv
+}
+
+function substract(lv, rv) {
+  return lv - rv
+}
+
+function multiply(lv, rv) {
+  return lv * rv
+}
+
+function divide(lv, rv) {
+  return lv / rv
+}
+
+

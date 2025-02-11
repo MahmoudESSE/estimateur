9a8da62fa43756ab74302d0ae88306c8230222db
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-02-11 02:04:54 +0100

    feat: add a function to evaluate the formula
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- js/script.js
+++ js/script.js
@@ -44,6 +44,29 @@ let currentValue = "";
 let previousValue = "";
 let operator = "";
 
+/**
+ * @param {number} lv
+ * @param {number} rv
+ * @param {string} op
+ * @returns {number}
+ */
+function operate(lv, rv, op) {
+  lv = Number.parseFloat(lv);
+  rv = Number.parseFloat(rv);
+  switch (op) {
+    case "+":
+      return add(lv, rv);
+    case "-":
+      return substract(lv, rv);
+    case "*":
+      return multiply(lv, rv);
+    case "/":
+      return divide(lv, rv);
+    default:
+      return rv;
+  }
+}
+
 const display = document.getElementById("display")
 const buttons = document.querySelectorAll("button")
 

2bc6f84a9211c16dd70e09a0b2076ca91e6929da
MahmoudESSE <mahmoudessehayli@gmail.com>
2025-02-11 02:02:35 +0100

    fix: add a divide by zero check in the divide function
    Signed-off-by: MahmoudESSE <mahmoudessehayli@gmail.com>

--- js/script.js
+++ js/script.js
@@ -1,17 +1,43 @@
+// TODO: add checks for each function to assert that we are operating on numbers
+
+/**
+ * @param {number} lv
+ * @param {number} rv
+ * @returns {number}
+ */
 function add(lv, rv) {
-  return lv + rv
+  return lv + rv;
 }
 
+/**
+ * @param {number} lv
+ * @param {number} rv
+ * @returns {number}
+ */
 function substract(lv, rv) {
-  return lv - rv
+  return lv - rv;
 }
 
+/**
+ * @param {number} lv
+ * @param {number} rv
+ * @returns {number}
+ */
 function multiply(lv, rv) {
-  return lv * rv
+  return lv * rv;
 }
 
+/**
+ * @param {number} lv
+ * @param {number} rv
+ * @return {number|null}
+ */
 function divide(lv, rv) {
-  return lv / rv
+  if (rv === 0) {
+    return null;
+  }
+
+  return lv / rv;
 }
 
 
